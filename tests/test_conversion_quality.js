89e7d4be14ec203438e20ae2cda2f75c0bc11f77
felipe11shimizu <felipe11shimizu@gmail.com>
2026-09-22 23:12:08 -0300

    test: align conversion quality metrics expectations

--- tests/test_conversion_quality.js
+++ tests/test_conversion_quality.js
@@ -6,7 +6,7 @@ const ConversionQuality = require('../frontend/modules/conversion_quality.js');
 
 assert.ok(ConversionQuality);
 assert.deepEqual(ConversionQuality.metrics('# T\n\ntexto'), {
-  characters: 8, lines: 3, headings: 1, tables: 0, links: 0, words: 2
+  characters: 10, lines: 3, headings: 1, tables: 0, links: 0, words: 3
 });
 assert.equal(ConversionQuality.diffScore('a', 'a'), 0);
 assert.equal(ConversionQuality.diffScore('a', 'b'), 100);
