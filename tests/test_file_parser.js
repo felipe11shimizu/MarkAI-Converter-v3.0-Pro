e09ce302dddf910e910edd1e5777a81e416bb80c
felipe11shimizu <felipe11shimizu@gmail.com>
2026-09-22 22:12:52 -0300

    test: fix CommonJS module assertions

--- tests/test_file_parser.js
+++ tests/test_file_parser.js
@@ -2,11 +2,11 @@
 
 const assert = require('node:assert/strict');
 
-require('../frontend/modules/file_parser.js');
+const FileParser = require('../frontend/modules/file_parser.js');
 
-assert.ok(global.MarkAIFileParser);
+assert.ok(FileParser);
 for (const method of ['parse', 'parseBrowser']) {
-  assert.equal(typeof global.MarkAIFileParser[method], 'function', method + ' should be exported');
+  assert.equal(typeof FileParser[method], 'function', method + ' should be exported');
 }
 
 console.log('file_parser module tests: ok');
