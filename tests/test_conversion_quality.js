97cacf6ddd745b88ada0571d48a5325eb2771d2e
felipe11shimizu <felipe11shimizu@gmail.com>
2026-09-22 22:13:02 -0300

    test: fix CommonJS module assertions

--- tests/test_conversion_quality.js
+++ tests/test_conversion_quality.js
@@ -1,10 +1,14 @@
 'use strict';
+
 const assert = require('node:assert/strict');
-require('../frontend/modules/conversion_quality.js');
-assert.ok(global.MarkAIConversionQuality);
-assert.deepEqual(global.MarkAIConversionQuality.metrics('# T\n\ntexto'), {
+
+const ConversionQuality = require('../frontend/modules/conversion_quality.js');
+
+assert.ok(ConversionQuality);
+assert.deepEqual(ConversionQuality.metrics('# T\n\ntexto'), {
   characters: 8, lines: 3, headings: 1, tables: 0, links: 0, words: 2
 });
-assert.equal(global.MarkAIConversionQuality.diffScore('a','a'), 0);
-assert.equal(global.MarkAIConversionQuality.diffScore('a','b'), 100);
+assert.equal(ConversionQuality.diffScore('a', 'a'), 0);
+assert.equal(ConversionQuality.diffScore('a', 'b'), 100);
+
 console.log('conversion_quality module tests: ok');
