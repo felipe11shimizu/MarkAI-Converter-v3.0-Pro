ad047aa43147bf90e2b1f33b91ae7d1b3ef2ee5d
felipe11shimizu <felipe11shimizu@gmail.com>
2026-09-22 22:12:58 -0300

    test: fix CommonJS module assertions

--- tests/test_chat_formatter.js
+++ tests/test_chat_formatter.js
@@ -1,9 +1,14 @@
 'use strict';
+
 const assert = require('node:assert/strict');
-require('../frontend/modules/chat_formatter.js');
-assert.ok(global.MarkAIChatFormatter);
-assert.equal(typeof global.MarkAIChatFormatter.format, 'function');
-const out = global.MarkAIChatFormatter.format('User: Olá\nAI: Olá!\n');
+
+const ChatFormatter = require('../frontend/modules/chat_formatter.js');
+
+assert.ok(ChatFormatter);
+assert.equal(typeof ChatFormatter.format, 'function');
+
+const out = ChatFormatter.format('User: Olá\nAI: Olá!\n');
 assert.match(out, /Usuário/);
 assert.match(out, /IA/);
+
 console.log('chat_formatter module tests: ok');
