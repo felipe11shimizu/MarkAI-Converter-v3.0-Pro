ff206f4e0cbe9b7c6ed23a983a342fcbf02e01bd
felipe11shimizu <felipe11shimizu@gmail.com>
2026-09-25 10:37:16 -0300

    fix(devtrail): explicitly install autonomous agent in service worker

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,6 @@
 importScripts('autonomous_agent.js');
 const devTrailAutonomousAgent = globalThis.DevTrailAutonomousAgent?.(chrome);
+devTrailAutonomousAgent?.install?.();
 
 importScripts('explorer/explorer_policy.js', 'explorer/explorer_state.js', 'explorer/explorer_engine.js');
 
